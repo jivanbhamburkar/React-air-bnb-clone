0f2287b5043374693b3f7ca7d71ad0d68517ce76
jivanbhamburkar <96575075+jivanbhamburkar@users.noreply.github.com>
2022-04-09 00:14:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          Deployment is working
-       </header>
+          changes are made
     </div>
   );
 }
