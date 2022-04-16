3d331556922a31a5a64ac6eec03b642a12562d09
jivanbhamburkar <96575075+jivanbhamburkar@users.noreply.github.com>
2022-04-17 03:56:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
+import ReactScrollWheelHandler from "react-scroll-wheel-handler";
 import Header from "./components/Header";
 import Header1 from "./components/Header1";
 import Help from "./components/Help";
@@ -10,34 +11,39 @@ import Cards from "./components/Cards";
 function App() {
   const [show, setShow] = useState(true);
 
-  const handleScroll = () => {
-    setShow(false);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <>
-      <div className="app">
-        {show ? (
-          <>
-            <Header1 />
-            <Search />
-          </>
-        ) : (
-          <Header show={show} setShow={setShow} />
-        )}
-        <Help />
-        <Image />
-      </div>
-      <div style={{ backgroundColor: "white" }}>
-        <Cards />
-      </div>
+      <ReactScrollWheelHandler
+        upHandler={(e) => {
+          if (e.layerY < 600) {
+            // console.log(e.layerY);
+            if (!show) setShow(true);
+          }
+        }}
+        downHandler={() => {
+          if (show) {
+            setShow(false);
+          }
+        }}
+      >
+        <div className="app">
+          {show ? (
+            <>
+              <Header1 />
+              <Search />
+            </>
+          ) : (
+            <Header show={show} setShow={setShow} />
+          )}
+
+          <Help />
+
+          <Image />
+        </div>
+        <div style={{ backgroundColor: "white" }}>
+          <Cards />
+        </div>
+      </ReactScrollWheelHandler>
     </>
   );
 }
